a3087eafc593df34d535e4e710eb091ac5579591
YanaBabarikova <yana.babarikova@gmail.com>
2024-10-05 14:23:09 +0300

    First hw

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,73 @@
 public class Main {
 
+        // Задача 1:
     public static void main(String[] args){
-        var box = 5;
-        System.out.println(box);
-        box = box + 2;
-        System.out.println(box);
-        box = box - 3;
-        System.out.println(box);
-        box = box * 3;
-        System.out.println(box);
-        box = box / 4;
-        System.out.println(box);
+       var dog = 8.0;
+       var cat = 3.6;
+       var paper = 763789;
 
-        var liftingCapacity = 50;
-        var stuffWaight = 20;
-        var сapacityLeft = liftingCapacity - stuffWaight;
+       System.out.println("dog: " + dog + " cat: " + cat + " paper: " + paper);
 
-        System.out.println("Ещё можно положить " + сapacityLeft + " кг вещей");
-        var appleWaeight = 2;
-        var orangeWaeight = 3;
-        var frutWaeight = appleWaeight + orangeWaeight;
-        System.out.println("Общий вес фруктов: " + frutWaeight +" кг");
+        // Задача 2:
+       dog = dog * 4;
+       cat = cat * 4;
+       paper = paper * 4;
 
-        var meatWaeight = 4;
-        var waterWeeight = 5;
-        var tomatoesWeight = 2;
-        var cucumberWeight = 2;
-        var peppersWeight = 2;
-        var zucchineWeight = 2;
-        var vegetableWeight =  tomatoesWeight + cucumberWeight + peppersWeight +zucchineWeight;
-        var productsWeight = vegetableWeight+ frutWaeight + meatWaeight + waterWeeight;
-        System.out.println("Общий вес продуктов: " + productsWeight + " кг");
+       System.out.println("Четыре собаки: " + dog + " Четыре кошки: " + cat + " Четыре бумаги: " + paper);
 
-        var leftWeight = liftingCapacity - stuffWaight - productsWeight;
-        System.out.println("Места осталось: " + leftWeight + " кг");
+        // Задача 3:
+        dog = dog / 3.5;
+        cat = cat / 1.6;
+        paper = paper / 7639;
 
-        productsWeight = productsWeight * 2;
-        System.out.println("Теперь вес продуктов: " + productsWeight + " кг");
+        System.out.println("Собак теперь: " + dog + " Кошек теперь: " + cat + " Бумаги теперь: " + paper);
 
-        leftWeight = liftingCapacity - stuffWaight - productsWeight;
-        System.out.println("Теперь места осталось: " + leftWeight + " кг");
+        // Задача 4:
+        var friend = 19;
+        System.out.println(friend);
 
-        var overLoad = ( stuffWaight + productsWeight ) % liftingCapacity;
-        System.out.println("Перегруз на " + overLoad + " кг");
+        friend = friend * 2;
+       System.out.println(friend);
 
-        var productsInOneCar = productsWeight / 2;
-        System.out.println("В одну машину влезает " + productsInOneCar + " кг");
+        friend = friend / 7;
+       System.out.println(friend);
+
+       // Задача 5:
+        var frog = 3.5;
+        System.out.println(frog);
+
+        frog = frog * 10;
+        System.out.println(frog);
+
+        frog = frog / 3.5;
+        System.out.println(frog);
+
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // Задача 6:
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+
+        var boxersWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес: " + boxersWeight);
+
+        var differenceWeight = (secondBoxerWeight + firstBoxerWeight) % firstBoxerWeight;
+        System.out.println("Разница в весе: " + differenceWeight);
+
+        // Задача 7
+
+        differenceWeight = firstBoxerWeight - secondBoxerWeight;
+        System.out.println("Разница в весе полученная вычетанием: " + differenceWeight);
+
+        // Задача 8
+        var totalTime = 640;
+        var workingHours = 8;
+
+        var worker = totalTime / workingHours;
+        System.out.println("Всего работников в компании: " + worker + " человек");
+
+        worker = worker + 94;
+        workingHours = worker * workingHours;
+        System.out.println("Если в компании работает " + worker + " человека, то всего " + workingHours + " часов работы может быть поделено между сотрудниками");
 }}
