401771f784424fa601a96120f3801f5086e81c15
YanaBabarikova <yana.babarikova@gmail.com>
2024-10-13 15:35:46 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 import java.util.Arrays;
 
 public class Main {
+    public static int leapYear(int year ){
+        // Задача 1
+        if (year%4 == 0){
+            System.out.println(year + " год является високосным");
+        }
+        else {
+            System.out.println(year + " год не високосный");
+        }
+        return year;
+    }
     public static void main(String[] args) {
 
-        //Задача 1
-      String firstName = "Ivan ";
-      String middleName = "Ivanovich ";
-      String lastName = "Ivanov ";
-      String fullName = lastName + firstName + middleName;
-      System.out.println(fullName);
+        leapYear(2024);
 
-      //Задача 2
-        String capitalLettersFullName = fullName.toUpperCase();
-        System.out.println( "Данные Ф.И. О. сотрудника для заполнения отчёта - " + capitalLettersFullName);
+        // Задача 2
 
-        //Задача 3
-        fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника - " + fullName);
+
+
+
 
     }
 }
