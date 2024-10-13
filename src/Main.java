0b77f302e3065df59e3ec541b1522706c02fb58f
YanaBabarikova <yana.babarikova@gmail.com>
2024-10-13 16:05:47 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,10 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientPhone == 1 && currentYear > 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
+        } else if (clientPhone == 0 && currentYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+                else {
+        System.out.println("Не удалось определить Вашу операционную систему");        }
         return clientPhone;
     }
     public static int deliveryDistance(int deliveryDistance){
@@ -45,10 +46,10 @@ public class Main {
         leapYear(2024);
 
         // Задача 2
-        phoneAndYear(0, 2017);
+        phoneAndYear(13, 2017);
 
         // Задача 3
-        deliveryDistance(110);
+        deliveryDistance(60);
 
 
 
