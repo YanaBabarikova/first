479ee70a8df260b088a63279a63bbacd9983a872
YanaBabarikova <yana.babarikova@gmail.com>
2024-10-19 16:01:01 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
        Author author1 = new Author("Name", "Surname");
+       Author author2 = new Author("Name", "Surname");
        Book book1 = new Book("Book", 1994, author1);
+       Book book2 = new Book("Book", 1994, author2);
        System.out.println(book1);
+       System.out.println(book2);
+
+       System.out.println(author1.equals(author2));
+       System.out.println(book1.equals(book2));
+
+
+
                }
 }
